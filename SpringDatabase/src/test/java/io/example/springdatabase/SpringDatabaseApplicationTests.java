23f07d1815e50c8c1e08c8301b5a46f86690bceb
Giacomo Barzaghi <giacomo.barzaghi@yahoo.com>
2023-10-30 19:41:52 +0100

    aggiustamento

--- SpringDatabase/src/test/java/io/example/springdatabase/SpringDatabaseApplicationTests.java
+++ SpringDatabase/src/test/java/io/example/springdatabase/SpringDatabaseApplicationTests.java
@@ -1,13 +1,13 @@
-package io.example.server;
+package io.example.springdatabase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class SpringDatabaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
